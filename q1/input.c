a0c4b9c6bcd4add413511cfccb7ae75b688eb342
clashrover <sagar99x@gmail.com>
2020-02-23 19:35:18 +0530

    first commit

---
+++ q1/input.c
@@ -0,0 +1,55 @@
+abc"def\"ghi"jkl/*mno*/pqrn
+abc"def\\"ghi"jkl/*mno*/pqr"stun
+
+abc"def
+ghi"jkl
+
+abc"def
+ghi
+jkl"mno/*pqr*/stu
+
+abc'defnghi'jkln
+abc'def
+gh/*i
+jk*/l/*\''mno/*pqr*/stu
+
+
+
+/*testing*/
+#include <stdio.h>
+
+int main(){
+	int x = 5;
+	int y = 6;
+	/*so this is a single line comment*/
+	int c = x+y;/*so how you doing*/hello
+	/*This is a multiline comment. There
+	are no nested /*comments. So remove these
+	and complete the*/assignment.
+	c = c/2;
+	x=x+y;
+	End of multiline comment*/
+	/*hello*/fsfdsdf
+	printf("heelllo
+		dwew/*ed\n
+		wed*/w\0ed\n");
+	printf("Calculation of  has been done  \n");
+	printf("But has not");
+	char c = '\n';
+	char j = '/*';
+	return 0;
+}
+
+abc/*def*/ghi	
+abc/*def*/sghi	
+abcs/*def*/ghi
+
+abc//defn
+
+abc/*def
+ghi*/jkl
+mno
+
+abc/*def
+nghi/
+n
---
+++ q1/out.c
@@ -0,0 +1,54 @@
+abc"def\"ghi"jkl pqrn
+abc"def\\"ghi"jkl/*mno*/pqr"stun
+
+abc"def
+ghi"jkl
+
+abc"def
+ghi
+jkl"mno stu
+
+abc'defnghi'jkln
+abc'def
+gh/*i
+jk*/l/*\''mno stu
+
+
+
+ 
+#include <stdio.h>
+
+int main(){
+	int x = 5;
+	int y = 6;
+	 
+	int c = x+y; hello
+	 
+
+assignment.
+	c = c/2;
+	x=x+y;
+	End of multiline comment*/
+	 fsfdsdf
+	printf("heelllo
+		dwew/*ed\n
+		wed*/w\0ed\n");
+	printf("Calculation of  has been done  \n");
+	printf("But has not");
+	char c = '\n';
+	char j = '/*';
+	return 0;
+}
+
+abc ghi	
+abc sghi	
+abcs ghi
+
+abc//defn
+
+abc 
+jkl
+mno
+
+abc 
+
